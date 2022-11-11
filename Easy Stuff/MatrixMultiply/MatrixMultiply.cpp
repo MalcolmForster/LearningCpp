b701edf58b0b46da507851c8c5bf2bef11721850
Malcolm <forster.malcolm@gmail.com>
2022-11-11 21:10:27 +1300

    Added matrix multiply

--- Easy Stuff/MatrixMultiply/MatrixMultiply.cpp
+++ Easy Stuff/MatrixMultiply/MatrixMultiply.cpp
@@ -11,14 +11,15 @@ int main() {
     std::string user_Input ="";
     std::cout << "Enter matrix A, seperate numbers by spaces, and press return to enter new line. Enter 'done' on a new line when finished" << std::endl;
 
-
     while (user_Input != "done"){
         std::cout  << "Line " << i << ": ";
         std::cin >> user_Input;
-        while(strchar(' ',user_Input) )
-        strchr(' ',user_Input)
+        while(strchar(' ',user_Input)){
+            strchr(' ',user_Input)
+            
+        }
         i += 1;
-    }    
+    }
     
 
 
